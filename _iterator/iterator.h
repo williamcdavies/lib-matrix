d9c6771222fe23344c5e7b22f5e115392c2a9b74
William Chuter-Davies <152814048+williamcdavies@users.noreply.github.com>
2024-10-21 08:33:12 -0700

    Rollback iterator.h

--- _iterator/iterator.h
+++ _iterator/iterator.h
@@ -9,8 +9,10 @@ class iterator
                 typedef const type_parameter& const_type_reference;
                 typedef type_parameter* type_pointer;
                 typedef const type_parameter* const_type_pointer;
+
+                
         private:
-                type_pointer _ptr;
+                type_pointer ptr = nullptr;
 };
 
 #endif // _LIBCPP_ITERATOR
