cad96d3537a9913a7788e02f3806a26e5606633b
William Chuter-Davies <152814048+williamcdavies@users.noreply.github.com>
2025-01-05 21:13:19 -0800

    Write array functionality for matrix.h

--- matrix.h
+++ matrix.h
@@ -23,16 +23,48 @@ struct matrix
         {       __elems_.swap(__m.__elems_);
         }
 
+        // iterators:
+
+        // capacity:
+        unsigned long size() const
+        {       return __elems_.size();
+        }
+        unsigned long max_size() const
+        {       return __elems_.max_size();
+        }
+        bool empty() const
+        {       return __elems_.empty();
+        }
+
+        // element access:
+        type_reference operator [](const unsigned long __n)
+        {       return __elems_[__n];
+        }
+        const_type_reference operator [](const unsigned long __n) const
+        {       return __elems_[__n];
+        }
         type_reference at(const unsigned long __row, const unsigned long __col)
         {       return __elems_.at(((__row - 1) * _N) + (__col - 1));
         }
         const_type_reference at(const unsigned long __row, const unsigned long __col) const 
         {       return __elems_.at(((__row - 1) * _N) + (__col - 1));
+        }  
+        type_reference front()
+        {       return __elems_.front();
         }
-        type_pointer data() noexcept 
+        const_type_reference front() const
+        {       return __elems_.front();
+        }
+        type_reference back()
+        {       return __elems_.back();
+        }
+        const_type_reference back() const
+        {       return __elems_.back();
+        }
+        type_pointer data() 
         {       return __elems_.data();
         }
-        const_type_pointer data() const noexcept 
+        const_type_pointer data() const 
         {       return __elems_.data();
         } 
 };
